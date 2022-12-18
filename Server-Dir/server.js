a3921dc53819342b7dac8049872d0e0b39dea15b
Mika Bibas <mikabibas@gmail.com>
2022-12-18 14:29:51 +0200

    feedback

--- Server-Dir/server.js
+++ Server-Dir/server.js
@@ -1,83 +1,83 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const {db} = require('./config/db.js')
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { db } = require("./config/db.js");
 
-const app = express()
+const app = express();
 
-dotenv.config
-app.use(cors())
+dotenv.config();
+app.use(cors());
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(process.env.PORT || 3001, () => {
-    console.log(`run on port ${process.env.PORT}||3001`);
-})
+  console.log(`run on port ${process.env.PORT}`);
+});
 
-app.get('/products/shoes', (req,res) => {
-    db('shoes')
-    .select('*')
-    .then(rows => {
-        res.json(rows)
+app.get("/products/shoes", (req, res) => {
+  db("shoes")
+    .select("*")
+    .then((rows) => {
+      res.json(rows);
     })
-    .catch(err => {
-        console.log(err);
-    })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.get('/products/harnesses', (req,res) => {
-    db('harnesses')
-    .select('*')
-    .then(rows => {
-        res.json(rows)
+app.get("/products/harnesses", (req, res) => {
+  db("harnesses")
+    .select("*")
+    .then((rows) => {
+      res.json(rows);
     })
-    .catch(err => {
-        console.log(err);
-    })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.get('/products/ropes', (req,res) => {
-    db('ropes')
-    .select('*')
-    .then(rows => {
-        res.json(rows)
+app.get("/products/ropes", (req, res) => {
+  db("ropes")
+    .select("*")
+    .then((rows) => {
+      res.json(rows);
     })
-    .catch(err => {
-        console.log(err);
-    })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.get('/products/helmets', (req,res) => {
-    db('helmets')
-    .select('*')
-    .then(rows => {
-        res.json(rows)
+app.get("/products/helmets", (req, res) => {
+  db("helmets")
+    .select("*")
+    .then((rows) => {
+      res.json(rows);
     })
-    .catch(err => {
-        console.log(err);
-    })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.post('/cart', (req,res) => {
-    db('cart')
+app.post("/cart", (req, res) => {
+  db("cart")
     .insert(req.body)
-    .returning('*')
-    .then(rows => {
-        res.json({msg:'added'})
-    }) 
-    .catch(e => {
-        console.log(e);
+    .returning("*")
+    .then((rows) => {
+      res.json({ msg: "added" });
     })
-})
+    .catch((e) => {
+      console.log(e);
+    });
+});
 
-app.get('/cart', (req,res) => {
-    db('cart')
-    .select('*')
-    .then(rows => {
-        res.json(rows)
+app.get("/cart", (req, res) => {
+  db("cart")
+    .select("*")
+    .then((rows) => {
+      res.json(rows);
     })
-    .catch(e => {
-        console.log(e);
-    })
-})
+    .catch((e) => {
+      console.log(e);
+    });
+});
--- src/components/MyCart.js
+++ src/components/MyCart.js
@@ -1,52 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class MyCart extends Component {
-    constructor() {
-        super()
-        this.state = {
-            products:[]
-        }
-    }
-    getProducts = async () => {
-    const call = await fetch('http://localhost:3001/cart')
-    const res = await call.json()
-    this.setState({products:res})
-}
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+  }
+  getProducts = async () => {
+    const call = await fetch("http://localhost:3001/cart");
+    const res = await call.json();
+    this.setState({ products: res });
+  };
 
-componentDidMount() {
-    this.getProducts() 
-}
+  componentDidMount() {
+    this.getProducts();
+  }
 
-
-
-
-    render() {
-        return (
-            <div>
-                <h2 className='tl'>My Cart</h2>
-                <div className='cartProducts'>
-                    <table>
-                        <th className='f3'>Product</th>
-                        <th className='f3'>Price</th>
-                        <th className='f3'>Qt</th>
-                        <tr>
-                        {
-                            this.state.products.map(item => {
-                                return(
-                                    <>
-                                    <td className='f4'>{item.name}</td>
-                                    <td className='f4'>{item.price}</td>
-                                    <td className='f4'>2</td>                
-                                    </>
-                                        )
-                                    })
-                                }
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2 className="tl">My Cart</h2>
+        <div className="cartProducts">
+          <table>
+            <th className="f3">Product</th>
+            <th className="f3">Price</th>
+            <th className="f3">Qt</th>
+            <tr>
+              {this.state.products.map((item) => {
+                return (
+                  <>
+                    <td className="f4">{item.name}</td>
+                    <td className="f4">{item.price}</td>
+                    <td className="f4">2</td>
+                  </>
+                );
+              })}
+            </tr>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MyCart;
